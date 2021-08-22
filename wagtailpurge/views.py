8e716bb6832da5fe1564e1b255dd8db69d48d617
Andy Babic <andyjbabic@gmail.com>
2021-08-22 21:52:06 +0100

    Wrap async process in transaction.on_commit() to avoid locking issues

--- wagtailpurge/views.py
+++ wagtailpurge/views.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from asgiref.sync import sync_to_async
+from django.db import transaction
 from django.shortcuts import redirect
 from django.utils.functional import cached_property
 from django.utils.text import capfirst
@@ -21,7 +22,7 @@ class PurgeRequestSubmitView(CreateView):
 
     def form_valid(self, form):
         resp = super().form_valid(form)
-        asyncio.run(process_purge_request(form.instance))
+        transaction.on_commit(lambda: asyncio.run(process_purge_request(form.instance)))
         return resp
 
     def get_success_message(self, instance):

6c689a9d481e5d31b34a35d8d0fb66580d7a7062
Andy Babic <andy.babic@torchbox.com>
2021-08-22 04:59:03 +0100

    Add dummy tests

---
+++ wagtailpurge/tests/test_models.py
@@ -0,0 +1,22 @@
+def test_djangocachepurgerequest_process_success():
+    pass
+
+
+def test_djangocachepurgerequest_process_fail():
+    pass
+
+
+def test_pageurlpurgerequest_process_success():
+    pass
+
+
+def test_pageurlpurgerequest_process_fail():
+    pass
+
+
+def test_imagerenditionspurgerequest_process_success():
+    pass
+
+
+def test_imagerenditionspurgerequest_process_fail():
+    pass
---
+++ wagtailpurge/tests/test_views.py
@@ -0,0 +1,6 @@
+def test_index_view():
+    pass
+
+
+def test_submit_view():
+    pass

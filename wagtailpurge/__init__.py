4b84b572d8991c2b8ebb148b99f8483c929ce1c1
Andy Babic <andyjbabic@gmail.com>
2021-11-11 14:31:08 +0000

    Release prep: v0.3 (#8)
    * Update trove classifiers
    
    * Test against latest Wagtail
    
    * Update install_requires for Wagtail
    
    * Update CHANGELOG
    
    * Update app version
    
    * Update README

--- wagtailpurge/__init__.py
+++ wagtailpurge/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Andy Babic"
 __author_email__ = "andyjbabic@gmail.com"
 __copyright__ = "Copyright 2021 Andy Babic"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def get_version():
